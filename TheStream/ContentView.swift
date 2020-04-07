475eb975dd985b347b5da26ce695445a691b9e5c
Jeff Taggart <jeff@jetaggart.com>
2020-04-07 11:50:57 -0600

    Initial Commit

---
+++ TheStream/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TheStream
+//
+//  Created by Jeff Taggart on 4/7/20.
+//  Copyright © 2020 Stream.io Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
